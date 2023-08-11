e6544c05f42bcfb10d30684bac425397a5e94fc0
Gali Kechichian <galialik.k@gmail.com>
2023-08-11 17:42:33 +0300

    fixed small bug

--- savify.py
+++ savify.py
@@ -27,34 +27,35 @@ song_link = st.text_input(
     placeholder="Example: https://open.spotify.com/track/58sxA5EEjq57yGggwThArx?si=edf556a1afcb4ab3",
     )
 
-with st.spinner("Loading your song..."):
-    try:
-        response = get_track(extract_track_id(song_link))
-        track_info = get_track_info(response)
-    except:
-        st.error('An error occurred white fetching the song. Please try again.')
+if song_link:
+    with st.spinner("Loading your song..."):
+        try:
+            response = get_track(extract_track_id(song_link))
+            track_info = get_track_info(response)
+        except:
+            st.error('An error occurred white fetching the song. Please try again.')
 
 
-if (got_error(response)):
-    st.error(track_info, icon="🚨")
-else:
-    st.caption(f"{', '.join(track_info['artists'])} - {track_info['name']}")
-    button_container = st.empty()
-    button_label = 'Convert to MP3'
-    if button_container.button(button_label):
-        with st.spinner("Converting audio..."):
-            try:
-                buffer = download_audio_to_buffer(track_info)
-                filesize = get_filesize(track_info)
-                filename = get_filename(track_info)
-                st.success(f"Conversion successful! File size: {filesize}Mb")
-                button_container.empty()
-                st.download_button(
-                    label="Download MP3 File",
-                    data=buffer,
-                    file_name=filename,
-                    mime="audio/mpeg"
-                )
-            except:
-                st.error('An error occurred. Please try again.')
-    
+    if (got_error(response)):
+        st.error(track_info, icon="🚨")
+    else:
+        st.caption(f"{', '.join(track_info['artists'])} - {track_info['name']}")
+        button_container = st.empty()
+        button_label = 'Convert to MP3'
+        if button_container.button(button_label):
+            with st.spinner("Converting audio..."):
+                try:
+                    buffer = download_audio_to_buffer(track_info)
+                    filesize = get_filesize(track_info)
+                    filename = get_filename(track_info)
+                    st.success(f"Conversion successful! File size: {filesize}Mb")
+                    button_container.empty()
+                    st.download_button(
+                        label="Download MP3 File",
+                        data=buffer,
+                        file_name=filename,
+                        mime="audio/mpeg"
+                    )
+                except:
+                    st.error('An error occurred. Please try again.')
+        
